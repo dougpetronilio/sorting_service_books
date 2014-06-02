bff12e74979d9f6a430d3e9d5a1f1d6c341d44f2
Douglas Petronilio <dougpetronilio@gmail.com>
2014-06-02 20:01:54 -0300

    arrumando software de test.

--- bin/test.rb
+++ bin/test.rb
@@ -1,24 +1,30 @@
 #!/usr/bin/env ruby
 
 require 'bundler/setup'
+$:.unshift File.join(File.dirname(__FILE__), "..", "lib")
 require "sorting_service_books"
 
 
-client = Client.new("../config_one_condition.yml")
+client = SortingServiceBooks::Client.new("config_one_condition.yml")
 puts "*"*50
-puts client.books
-puts client.sort
+client.books.each { |c| puts c.title }
+puts "-"*50
+client.sort.each { |c| puts c.title }
 puts "*"*50
+puts
 
-client = Client.new("../config_two_conditions.yml")
+client = SortingServiceBooks::Client.new("config_two_conditions.yml")
 puts "*"*50
-puts client.books
-puts client.sort
+client.books.each { |c| puts c.title }
+puts "-"*50
+client.sort.each { |c| puts c.title }
 puts "*"*50
+puts
 
-client = Client.new("../config_three_conditions.yml")
+client = SortingServiceBooks::Client.new("config_three_conditions.yml")
 puts "*"*50
-puts client.books
-puts client.sort
+client.books.each { |c| puts c.title }
+puts "-"*50
+client.sort.each { |c| puts c.title }
 puts "*"*50
 
